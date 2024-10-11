5265249ac30130d84ccdb7d3845bf2fa04d57776
Abhishek Mishra <cs.abhishekmo@gmail.com>
2024-10-11 11:57:17 +0530

    init

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import "../css/navbar.css";
+import React from "react";
+import gdsc from "../Assets/img/gdsc-gif.gif";
+import { Link } from "react-router-dom";
+export default function NavBar() {
+  return (
+    <nav className="_navbar sticky inter-4">
+      <div className="navlogo">
+        <img style={{ height: "1%", width: "40%" }} src={gdsc} halt="logo" />
+      </div>
+
+      <ul className="px-4">
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className="hamburger">
+          {" "}
+          ☰{" "}
+        </label>
+        <div className="menu">
+          <li>
+            <Link className="link_text font-face-Bold " to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="link_text font-face-Bold " to="/teams">
+              Team
+            </Link>
+          </li>
+          <li>
+            <Link className="link_text font-face-Bold " to="/event">
+              Events
+            </Link>
+          </li>
+          <li>
+              <Link className="link_text font-face-Bold "  to="/blogs">Blogs</Link>
+            </li>
+
+          <li>
+            <a className="joinus inter-4" href="https://gdg.community.dev/gdg-on-campus-school-of-information-technology-bhopal-india/">
+              Join Us
+            </a>
+          </li>
+        </div>
+      </ul>
+
+
+    </nav>
+    
+  );
+}
