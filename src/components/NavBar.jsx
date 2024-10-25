bcc16b37c31601cdf56dacbc18aacacd5c55ea51
Nilesh Dhakad <118746301+nileshdhakad18@users.noreply.github.com>
2024-10-25 09:24:52 +0530

    demo pr

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ export default function NavBar() {
             </li>
 
           <li>
-            <a className="joinus inter-4" href="https://gdg.community.dev/gdg-on-campus-school-of-information-technology-bhopal-india/" target="_blank" >
+            <a className="joinus inter-4" href="https://gdg.community.dev/gdg-on-campus-school-of-information-technology-bhopal-india/"  >
               Join Us
             </a>
           </li>
